f7ef1acfe922e5dc48990856cdf651d05de7424d
Rocio Jasmin Menjivar Menjivar <mm0588032021@unab.edu.sv>
2021-08-25 22:08:09 -0600

    Notas_promedio
    2 capas

---
+++ Promedio2/Promedio2/Negocio/NClsNotas1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio2.Negocio
+{
+    class NClsNotas1
+    {
+        private double laboratorio1;
+        private double parcial01;
+        private double promedio01;
+        private double laboratorio02;
+        private double parcial02;
+        private double promedio02;
+        private double laboratorio03;
+        private double parcial03;
+        private double promedi03;
+        private double nota_final;
+
+        
+
+        public double Laboratorio1 { get => laboratorio1; set => laboratorio1 = value; }
+        public double Parcial01 { get => parcial01; set => parcial01 = value; }
+        public double Promedio01 { get => promedio01; set => promedio01 = value; }
+        public double Laboratorio02 { get => laboratorio02; set => laboratorio02 = value; }
+        public double Parcial02 { get => parcial02; set => parcial02 = value; }
+        public double Promedio02 { get => promedio02; set => promedio02 = value; }
+        public double Laboratorio03 { get => laboratorio03; set => laboratorio03 = value; }
+        public double Parcial03 { get => parcial03; set => parcial03 = value; }
+        public double Promedi03 { get => promedi03; set => promedi03 = value; }
+        public double Nota_final { get => nota_final; set => nota_final = value; }
+
+        public NClsNotas1(double laboratorio1, double parcial01, double laboratorio02, double parcial02, double laboratorio03, double parcial03)
+        {
+            this.laboratorio1 = laboratorio1;
+            this.parcial01 = parcial01;
+           
+            this.laboratorio02 = laboratorio02;
+            this.parcial02 = parcial02;
+           
+            this.laboratorio03 = laboratorio03;
+            this.parcial03 = parcial03;
+
+            promedio01 = (laboratorio1 * 0.40) + (parcial01*0.60);
+            promedio02 = (laboratorio02 * 0.40) + (parcial02*0.60);
+            promedi03 = (laboratorio03 * 0.40) + (parcial03 * 0.60);
+
+            nota_final = (promedio01 + promedio02 + promedi03) / 3;
+
+
+        }
+
+        public override string ToString()
+        {
+            return "Su promedio obtedino del periodo 1 es :" + promedio01 + "\n" +
+                   "Su promedio obtedino del periodo 2 es :" + promedio02 + "\n" +
+                   "Su promedio obtedino del periodo 3 es :" + promedi03 + "\n" +
+                   "El Promedio total  es :" + nota_final;
+
+
+        }
+    }
+
+    
+}
---
+++ Promedio2/Promedio2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Promedio2.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NClsNotas1 notas = new NClsNotas1(9, 10, 8, 9, 8, 9);
+
+            Console.WriteLine(notas.ToString());
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
