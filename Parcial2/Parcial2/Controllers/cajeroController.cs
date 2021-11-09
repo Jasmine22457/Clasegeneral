661e7a06b4b38c5aad07d2ec8744fbe87cae6e76
Jas Menjivar <mm0588032021@unab.edu.sv>
2021-11-09 00:01:13 -0600

    Parcial2

---
+++ Parcial2/Parcial2/Controllers/cajeroController.cs
@@ -0,0 +1,55 @@
+﻿using Parcial2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial2.Controllers
+{
+    public class cajeroController : Controller
+    {
+
+        [HttpGet]
+
+        public ActionResult index1()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult index1(Cajero cajero)
+        {
+            if (cajero.Cantidad % 5 == 0)
+            {
+
+                return Redirect("MostrarDatos");
+
+            }
+            else
+            {
+
+                return Redirect("Error");
+            }
+
+        }
+
+
+        [HttpGet]
+
+        public ActionResult Error()
+        {
+
+            return View();
+        }
+        [HttpGet]
+
+        public ActionResult MostrarDatos()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Parcial2/Parcial2/Models/Cajero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class Cajero
+    {
+        public int Cantidad { get; set; }
+    }
+}
